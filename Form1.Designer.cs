74621f91c6fccd73ca63b478501c4d14b891fae0
aki <aki.index@gmail.com>
2022-02-11 08:48:12 +0900

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace SystemInfo;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        /// <summary>
+        /// lblDate
+        /// </summary>
+        /// <returns></returns>
+        this.lblDate = new System.Windows.Forms.Label();
+        this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblDate.Font = new Font("BIZ UDPゴシック", 24, FontStyle.Bold);
+        this.lblDate.Location = new System.Drawing.Point(20, 10);
+        this.lblDate.Name = "NowDate";
+        this.lblDate.Size = new System.Drawing.Size(360, 60);
+        this.lblDate.BackColor = System.Drawing.Color.Black;
+        this.lblDate.ForeColor = System.Drawing.Color.White;
+        this.lblDate.Text = System.DateTime.Now.ToShortDateString();
+        this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblDate.Click += LblDate_Click;
+
+        /// <summary>
+        /// lblTime
+        /// </summary>
+        /// <returns></returns>
+        this.lblTime = new System.Windows.Forms.Label();
+        this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblTime.Font = new Font("BIZ UDPゴシック", 24);
+        this.lblTime.Location = new System.Drawing.Point(20, 70);
+        this.lblTime.Name = "NowTime";
+        this.lblTime.Size = new System.Drawing.Size(350, 60);
+        this.lblTime.BackColor = System.Drawing.Color.Black;
+        this.lblTime.ForeColor = System.Drawing.Color.Gold;
+        this.lblTime.Text = System.DateTime.Now.ToShortTimeString();
+        this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+
+        /// <summary>
+        /// lblBattery
+        /// </summary>
+        /// <returns></returns>
+        this.lblBattery = new System.Windows.Forms.Label();
+        this.lblBattery.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblBattery.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblBattery.Location = new System.Drawing.Point(30, 500);
+        this.lblBattery.Name = "Batt";
+        this.lblBattery.Size = new System.Drawing.Size(340, 48);
+        this.lblBattery.BackColor = System.Drawing.Color.Transparent;
+        this.lblBattery.ForeColor = System.Drawing.Color.White;
+        this.lblBattery.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+        /// <summary>
+        /// lblBatteryValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblBatteryValue = new System.Windows.Forms.Label();
+        this.lblBatteryValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblBatteryValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblBatteryValue.Location = new System.Drawing.Point(0, 0);
+        this.lblBatteryValue.Name = "BattValue";
+        this.lblBatteryValue.Size = new System.Drawing.Size(340, 48);
+        this.lblBatteryValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblBatteryValue.ForeColor = System.Drawing.Color.White;
+        this.lblBatteryValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblBattery.Controls.Add(this.lblBatteryValue);
+
+        /// <summary>
+        /// lblCpu
+        /// </summary>
+        /// <returns></returns>
+        this.lblCpu = new System.Windows.Forms.Label();
+        this.lblCpu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblCpu.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblCpu.Location = new System.Drawing.Point(30, 547);
+        this.lblCpu.Name = "CPU";
+        this.lblCpu.Size = new System.Drawing.Size(340, 48);
+        this.lblCpu.BackColor = System.Drawing.Color.Transparent;
+        this.lblCpu.ForeColor = System.Drawing.Color.White;
+        this.lblCpu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblCpu.Text = " CPU ";
+
+        /// <summary>
+        /// lblCpuValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblCpuValue = new System.Windows.Forms.Label();
+        this.lblCpuValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblCpuValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblCpuValue.Location = new System.Drawing.Point(0, 0);
+        this.lblCpuValue.Name = "CPUValue";
+        this.lblCpuValue.Size = new System.Drawing.Size(340, 48);
+        this.lblCpuValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblCpuValue.ForeColor = System.Drawing.Color.White;
+        this.lblCpuValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblCpu.Controls.Add(this.lblCpuValue);
+  
+        /// <summary>
+        /// lblMem
+        /// </summary>
+        /// <returns></returns>
+        this.lblMem = new System.Windows.Forms.Label();
+        this.lblMem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblMem.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblMem.Location = new System.Drawing.Point(30, 594);
+        this.lblMem.Name = "MEM";
+        this.lblMem.Size = new System.Drawing.Size(340, 48);
+        this.lblMem.BackColor = System.Drawing.Color.Transparent;
+        this.lblMem.ForeColor = System.Drawing.Color.White;
+        this.lblMem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblMem.Text = " MEM ";
+
+        /// <summary>
+        /// lblMemValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblMemValue = new System.Windows.Forms.Label();
+        this.lblMemValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblMemValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblMemValue.Location = new System.Drawing.Point(0, 0);
+        this.lblMemValue.Name = "MEMValue";
+        this.lblMemValue.Size = new System.Drawing.Size(340, 48);
+        this.lblMemValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblMemValue.ForeColor = System.Drawing.Color.White;
+        this.lblMemValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblMem.Controls.Add(this.lblMemValue);
+
+        /// <summary>
+        /// lblDisk
+        /// </summary>
+        /// <returns></returns>
+        this.lblDisk = new System.Windows.Forms.Label();
+        this.lblDisk.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblDisk.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblDisk.Location = new System.Drawing.Point(30, 641);
+        this.lblDisk.Name = "DISK";
+        this.lblDisk.Size = new System.Drawing.Size(340, 48);
+        this.lblDisk.BackColor = System.Drawing.Color.Transparent;
+        this.lblDisk.ForeColor = System.Drawing.Color.White;
+        this.lblDisk.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblDisk.Text = " DISK ";
+
+        /// <summary>
+        /// lblDiskValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblDiskValue = new System.Windows.Forms.Label();
+        this.lblDiskValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblDiskValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblDiskValue.Location = new System.Drawing.Point(0, 0);
+        this.lblDiskValue.Name = "DISKValue";
+        this.lblDiskValue.Size = new System.Drawing.Size(340, 48);
+        this.lblDiskValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblDiskValue.ForeColor = System.Drawing.Color.White;
+        this.lblDiskValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblDisk.Controls.Add(this.lblDiskValue);
+
+        /// <summary>
+        /// lblNet
+        /// </summary>
+        /// <returns></returns>
+        this.lblNet = new System.Windows.Forms.Label();
+        this.lblNet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblNet.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblNet.Location = new System.Drawing.Point(30, 688);
+        this.lblNet.Name = "Net";
+        this.lblNet.Size = new System.Drawing.Size(340, 48);
+        this.lblNet.BackColor = System.Drawing.Color.Transparent;
+        this.lblNet.ForeColor = System.Drawing.Color.White;
+        this.lblNet.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblNet.Text = " NET ";
+
+        /// <summary>
+        /// lblNetValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblNetValue = new System.Windows.Forms.Label();
+        this.lblNetValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblNetValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblNetValue.Location = new System.Drawing.Point(0, 0);
+        this.lblNetValue.Name = "NetValue";
+        this.lblNetValue.Size = new System.Drawing.Size(340, 48);
+        this.lblNetValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblNetValue.ForeColor = System.Drawing.Color.White;
+        this.lblNetValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblNet.Controls.Add(this.lblNetValue);
+
+        /// <summary>
+        /// lblVol
+        /// </summary>
+        /// <returns></returns>
+        this.lblVol = new System.Windows.Forms.Label();
+        this.lblVol.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblVol.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblVol.Location = new System.Drawing.Point(30, 735);
+        this.lblVol.Name = "Vol";
+        this.lblVol.Size = new System.Drawing.Size(340, 48);
+        this.lblVol.BackColor = System.Drawing.Color.Transparent;
+        this.lblVol.ForeColor = System.Drawing.Color.White;
+        this.lblVol.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        this.lblVol.Text = " VOL ";
+
+        /// <summary>
+        /// lblVolValue
+        /// </summary>
+        /// <returns></returns>
+        this.lblVolValue = new System.Windows.Forms.Label();
+        this.lblVolValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.lblVolValue.Font = new Font("BIZ UDPゴシック", 8);
+        this.lblVolValue.Location = new System.Drawing.Point(0, 0);
+        this.lblVolValue.Name = "VolValue";
+        this.lblVolValue.Size = new System.Drawing.Size(340, 48);
+        this.lblVolValue.BackColor = System.Drawing.Color.Transparent;
+        this.lblVolValue.ForeColor = System.Drawing.Color.White;
+        this.lblVolValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+        this.lblVol.Controls.Add(this.lblVolValue);
+
+        /// <summary>
+        /// Form
+        /// </summary>
+        /// <returns></returns>
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(400, 850);
+        this.Text = "System Info";
+        this.Opacity = 0.80;
+        this.ControlBox = false;
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.BackColor = System.Drawing.Color.Black;
+        this.ShowInTaskbar = false;
+        this.DoubleClick += Form_DoubleClick;
+
+        this.Controls.Add(this.lblDate);
+        this.Controls.Add(this.lblTime);
+        this.Controls.Add(this.lblBattery);
+        this.Controls.Add(this.lblCpu);
+        this.Controls.Add(this.lblMem);
+        this.Controls.Add(this.lblDisk);
+        this.Controls.Add(this.lblNet);
+        this.Controls.Add(this.lblVol);
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lblDate;
+    private System.Windows.Forms.Label lblTime;
+    private System.Windows.Forms.Label lblBattery;
+    private System.Windows.Forms.Label lblBatteryValue;
+    private System.Windows.Forms.Label lblCpu;
+    private System.Windows.Forms.Label lblCpuValue;
+    private System.Windows.Forms.Label lblMem;
+    private System.Windows.Forms.Label lblMemValue;
+    private System.Windows.Forms.Label lblDisk;
+    private System.Windows.Forms.Label lblDiskValue;
+    private System.Windows.Forms.Label lblNet;
+    private System.Windows.Forms.Label lblNetValue;
+    private System.Windows.Forms.Label lblVol;
+    private System.Windows.Forms.Label lblVolValue;
+}
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+namespace SystemInfo;
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Management;
+using System.Net.NetworkInformation;
+public partial class Form1 : Form
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Form1()
+    {
+        //各コントロール初期化
+        InitializeComponent();
+
+        //タイマーイベント作成
+        Timer timer = new Timer(this.components);
+        timer.Tick += new EventHandler(Timer_Tick);
+        timer.Interval = 5000;  //ミリ秒
+        timer.Enabled = true;
+ 
+        //日本の祝日取得
+        Dictionary<DateTime, string> dicHoliday = HolidayUtil();
+        //カレンダー設定／表示
+        SetCalendar(dicHoliday);
+    }
+
+    /// <summary>
+    /// フォームダブルクリックイベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Form_DoubleClick(object sender, EventArgs e) {
+        //ダブルクリックする度に画面をずらす
+        if (this.Top != 0) {
+            this.Top = 0;
+            this.Left = 0;
+        } else {
+            this.Top = -1;
+            this.Left = -1631;
+        }
+    }
+
+    /// <summary>
+    /// タイマーイベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Timer_Tick(object? sender, EventArgs e) {
+        //日付、時間を更新
+        this.lblDate.Text = DateTime.Now.ToShortDateString();
+        this.lblTime.Text = DateTime.Now.ToShortTimeString();
+    
+        //AC電源の状態取得
+        PowerLineStatus pls = SystemInformation.PowerStatus.PowerLineStatus;
+        if (pls == PowerLineStatus.Offline) {
+            this.lblBattery.Text = " BATT ";
+        } else if (pls == PowerLineStatus.Online) {
+            this.lblBattery.Text = " AC   ";
+        } else {
+            this.lblBattery.Text = " xxxx ";
+        }
+        float blp = SystemInformation.PowerStatus.BatteryLifePercent;       //バッテリー残量（割合）
+        int blr = SystemInformation.PowerStatus.BatteryLifeRemaining / 60;  //バッテリー残量（時間）
+        this.lblBatteryValue.Text = string.Format("{0:0.0} %   {1} 分 ", blp * 100, blr);
+
+        //CPU使用率取得
+        var searcher = new ManagementObjectSearcher("select LoadPercentage from CIM_Processor");
+        foreach (var obj in searcher.Get()) {
+            int cpuUse = 0;
+            try {
+                cpuUse = int.Parse((obj["LoadPercentage"]).ToString());
+            } catch {}
+            this.lblCpuValue.Text = string.Format("{0} % ", cpuUse);
+        }
+
+        //メモリ使用率取得
+        ManagementClass mc = new ManagementClass("Win32_OperatingSystem");
+        ManagementObjectCollection moc = mc.GetInstances();
+        foreach (ManagementObject mo in moc) {
+            float totalMemory = 0;
+            float freeMemory = 0;
+            try {
+                freeMemory= float.Parse(mo["FreePhysicalMemory"].ToString()) / 1024 / 1024;
+                totalMemory = float.Parse(mo["TotalVisibleMemorySize"].ToString()) / 1024 / 1024;
+            } catch {}
+
+            this.lblMemValue.Text = string.Format("{0:#.0} GB  /  {1:#.0} GB ", freeMemory, totalMemory);
+        }
+        moc.Dispose();
+        mc.Dispose();
+
+        //C:ドライブの情報を取得する
+        DriveInfo drive = new DriveInfo("C");
+        float totalSize = 0;
+        float totalFreeSize = 0;
+        //ドライブの準備ができているか調べる
+        if (drive.IsReady) {
+            totalSize = ((float)drive.TotalSize / 1024 / 1024 / 1024);
+            totalFreeSize = ((float)drive.TotalFreeSpace / 1024 / 1024 / 1024);
+        }
+        this.lblDiskValue.Text = string.Format("{0:#,##0.0} GB  /  {1:#,##0.0} GB ", totalFreeSize, totalSize);
+        
+        //ネットワーク速度
+        long previousbytessend = 0;
+        long previousbytesreceived = 0;
+        long downloadspeed = 0;
+        long uploadspeed = 0;
+        IPv4InterfaceStatistics interfaceStats;
+        for (int i = 0; i < NetworkInterface.GetAllNetworkInterfaces().Length; i++ ) {
+            if (NetworkInterface.GetAllNetworkInterfaces()[i].Description.Contains("Wireless Adapter")) {
+                interfaceStats = NetworkInterface.GetAllNetworkInterfaces()[i].GetIPv4Statistics();
+                previousbytessend = interfaceStats.BytesSent;
+                previousbytesreceived = interfaceStats.BytesReceived;
+                //Task.Run(async () => { await Task.Delay(5000); Console.WriteLine("待機終了"); });
+                Thread.Sleep(1000);
+                interfaceStats = NetworkInterface.GetAllNetworkInterfaces()[i].GetIPv4Statistics();
+                uploadspeed = (interfaceStats.BytesSent - previousbytessend) / 1024; //In KB/s
+                downloadspeed = (interfaceStats.BytesReceived - previousbytesreceived) / 1024;
+
+            }
+        }
+        this.lblNetValue.Text = string.Format("UP {0:0.0} MB/s   DL {1:0.0} MB/s ", (float)uploadspeed / 1024, (float)downloadspeed / 1024);
+        this.lblNetValue.Update();
+
+        //ボリューム取得
+        this.lblVolValue.Text = " 0 ";
+
+    }
+
+    /// <summary>
+    /// 日付クリックイベント
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void LblDate_Click(object sender, EventArgs e) {
+        Application.Exit();
+    }
+
+    /// <summary>
+    /// カレンダー設定／表示
+    /// </summary>
+    /// <param name="dicHoliday">日本の祝日（取得できない場合はnull）</param>
+    private void SetCalendar(Dictionary<DateTime, string> dicHoliday) {
+
+        //カレンダータイトル表示
+        string[] weekTitle = new string[] {"日","月","火","水","木","金","土"};
+        for (int i = 0; i < 7; i++) {
+            Label CalendarTitle = new Label();
+            CalendarTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            CalendarTitle.Font = new Font("BIZ UDPゴシック", 16);
+            CalendarTitle.Location = new System.Drawing.Point(30+i*48, 150);
+            CalendarTitle.Name = "CalendarTitle";
+            CalendarTitle.Size = new System.Drawing.Size(48, 48);
+            CalendarTitle.BackColor = System.Drawing.Color.Black;
+            CalendarTitle.ForeColor = System.Drawing.Color.White;
+            CalendarTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight; 
+            this.Controls.Add(CalendarTitle);
+
+            if (weekTitle[i] == "日") {
+                CalendarTitle.ForeColor = Color.Red;
+            } else if (weekTitle[i] == "土") {
+                CalendarTitle.ForeColor = Color.DeepSkyBlue;
+            } else {
+                CalendarTitle.ForeColor = Color.White;
+            }
+
+            CalendarTitle.Text = weekTitle[i];
+        }
+
+        //現在の日付を取得から当月1日を表すインスタンスを生成します
+        DateTime now1 = new DateTime( DateTime.Now.Year, DateTime.Now.Month, 1 );
+        //当月1日からその曜日分だけ引きます。
+        DateTime curDay = now1.AddDays( -( int )now1.DayOfWeek );
+
+        //日付を表示
+        for (int j = 0; j < 6; j++) {
+            for (int i = 0; i < 7; i++) {
+                Label CalendarDay = new Label();
+                CalendarDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                CalendarDay.Font = new Font("BIZ UDPゴシック", 12);
+                CalendarDay.Location = new System.Drawing.Point(30+i*48, 198+j*48);
+                CalendarDay.Name = "CalendarDay";
+                CalendarDay.Size = new System.Drawing.Size(48, 48);
+                CalendarDay.BackColor = System.Drawing.Color.Black;
+                CalendarDay.TextAlign = System.Drawing.ContentAlignment.MiddleRight; 
+                this.Controls.Add(CalendarDay);
+
+                //基本の色を設定
+                CalendarDay.Text = curDay.Day.ToString();
+                if (curDay.DayOfWeek == DayOfWeek.Sunday) {
+                    CalendarDay.ForeColor = Color.Red;
+                } else if (curDay.DayOfWeek == DayOfWeek.Saturday) {
+                    CalendarDay.ForeColor = Color.DeepSkyBlue;
+                } else {
+                    CalendarDay.ForeColor = Color.White;
+                }
+
+                //先月なら色を薄くする 
+                if (curDay.Month != DateTime.Now.Month) {
+                    CalendarDay.ForeColor = Color.SlateGray;
+                }
+                //当日なら色を付ける
+                if (curDay.Date == DateTime.Now.Date) {
+                    CalendarDay.BackColor = Color.DarkOliveGreen;
+                }
+
+                //祝日チェック
+                if (dicHoliday != null) {
+                    if (dicHoliday.ContainsKey(curDay.Date)) {
+                        CalendarDay.ForeColor = Color.Coral;
+                    }
+                }
+
+                //curDayを1日分進める
+                curDay = curDay.AddDays(1.0);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 日本の祝日取得
+    /// 内閣府から取得
+    /// </summary>
+    /// <returns>日本の祝日（取得できない場合はnullを返す）</returns>
+    private Dictionary<DateTime, string> HolidayUtil()
+    {
+        Dictionary<DateTime, string> dicHoliday = new Dictionary<DateTime, string>();
+
+        //.Net5でSJISを使う場合に必要
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        //ファイルの中身を取得
+        string _path = @"https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv";
+
+        var client = new System.Net.WebClient();
+        byte[] buffer = null;
+        try {
+            buffer = client.DownloadData(_path);
+            string str = Encoding.GetEncoding("shift_jis").GetString(buffer);
+            //行毎に配列に分割
+            string[] rows = str.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            //一行目を飛ばしてデータをディクショナリに格納
+            rows.Skip(1).ToList().ForEach(row =>
+            {
+                var cols = row.Split(',');
+                dicHoliday.Add(DateTime.Parse(cols[0]), cols[1]);
+            });
+
+        } catch {
+            dicHoliday = null;
+        }
+
+        return dicHoliday;
+    }
+}
